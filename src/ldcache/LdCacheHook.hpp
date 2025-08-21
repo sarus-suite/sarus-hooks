351b0a0f0360ce2e60f1d9a1b0e3305808852a85
felipecr <cruz@cscs.ch>
2025-08-22 00:05:04 +0200

    Implemented a relative ld cache refresh

--- src/ldcache/LdCacheHook.hpp
+++ src/ldcache/LdCacheHook.hpp
@@ -1,7 +1,10 @@
 #ifndef sarus_hooks_ldcache_LdCacheHook_hpp
 #define sarus_hooks_ldcache_LdCacheHook_hpp
 
+#include <string>
 #include <boost/filesystem.hpp>
+
+#include "libsarus/Utility.hpp"
 #include "libsarus/Logger.hpp"
 
 namespace sarus {
@@ -10,8 +13,7 @@ namespace ldcache {
 
 class LdCacheHook {
 public:
-    LdCacheHook();
-    void refresh();
+    void activate();
 
 private:
     void parseConfigJSONOfBundle();
@@ -19,7 +21,8 @@ private:
     void log(const std::string& msg, libsarus::LogLevel lvl) const;
 
 private:
-    boost::filesystem::path rootfsDir;
+    libsarus::hook::ContainerState containerState;
+	boost::filesystem::path rootfsDir;
     boost::filesystem::path ldconfigPath;
 };
 
--- src/ldcache/main.cpp
+++ src/ldcache/main.cpp
@@ -4,11 +4,11 @@
 
 int main(int argc, char* argv[]){
     try {
-        sarus::hooks::ldcache::LdCacheHook{}.refresh();
+        sarus::hooks::ldcache::LdCacheHook{}.activate();
 	}
-	catch (const libsarus::Error& e) {
+	catch (const libsarus::Error &e) {
 	  libsarus::Logger::getInstance().logErrorTrace(e, "ldcache-hook");
-	  return EXIT_FAILURE;
+	  exit(EXIT_FAILURE);
 	}
 	return 0;
 }
